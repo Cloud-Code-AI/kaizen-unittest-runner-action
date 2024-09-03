60300181279d610846cdf216ddb9ff0da0f651a2
Saurav Panda <sgp65@cornell.edu>
2024-09-02 22:02:28 -0700

    updated the file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,28 +7,31 @@ source /opt/venv/bin/activate
 
 # Run Python tests using kaizen-cli
 echo "Running Python tests..."
-kaizen-cli run tests .kaizen/unittests
+kaizen-cli unit-test run-tests
+
+# Add a step to log the error of log run to our server
+
 
 # Run JavaScript/TypeScript tests only if package.json has content
-if [ -s package.json ]; then
-    echo "Running JavaScript/TypeScript tests..."
-    if [ ! -f package-lock.json ]; then
-        npm install --package-lock-only
-    fi
-    npm ci
-    npm test
-else
-    echo "No package.json found or it's empty. Skipping JavaScript/TypeScript tests."
-fi
+# if [ -s package.json ]; then
+#     echo "Running JavaScript/TypeScript tests..."
+#     if [ ! -f package-lock.json ]; then
+#         npm install --package-lock-only
+#     fi
+#     npm ci
+#     npm test
+# else
+#     echo "No package.json found or it's empty. Skipping JavaScript/TypeScript tests."
+# fi
 
-# Run React tests only if package.json has content and includes react-scripts
-if [ -s package.json ] && grep -q "react-scripts" "package.json"; then
-    echo "Running React tests..."
-    if [ ! -f package-lock.json ]; then
-        npm install --package-lock-only
-    fi
-    npm ci
-    npm run test
-else
-    echo "No React project detected or package.json is missing/empty. Skipping React tests."
-fi
+# # Run React tests only if package.json has content and includes react-scripts
+# if [ -s package.json ] && grep -q "react-scripts" "package.json"; then
+#     echo "Running React tests..."
+#     if [ ! -f package-lock.json ]; then
+#         npm install --package-lock-only
+#     fi
+#     npm ci
+#     npm run test
+# else
+#     echo "No React project detected or package.json is missing/empty. Skipping React tests."
+# fi
